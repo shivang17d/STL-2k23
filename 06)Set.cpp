4f6a9ad8deba8dda848df3394e9ee254517b19e5
Shivang Gupta <shivangguptaxd@gmail.com>
2023-07-08 06:53:14 -0700

    Update 06)Set.cpp

--- 06)Set.cpp
+++ 06)Set.cpp
@@ -64,3 +64,43 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<string> s;
+    s.insert("abc");
+    s.insert("xyz");
+    s.insert("def");
+    s.insert("abc");
+    s.insert("abc");
+    s.insert("abc");
+    
+    auto it = s.find("abc");
+    if(it != s.end()){
+        s.erase(it);
+    }
+    
+    cout<<"Output"<<endl;
+    for(auto &val : s) {
+        cout<<val<<endl;
+    }
+    
+    s.erase("def");
+   
+    cout<<"Output"<<endl;
+    for(auto &val : s) {
+        cout<<val<<endl;
+    }
+
+    return 0;
+}
