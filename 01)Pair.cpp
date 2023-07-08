7fcbd968a9fe54787e640b82919abad3ee85bce6
Shivang Gupta <shivangguptaxd@gmail.com>
2023-07-08 10:03:05 +0530

    Create 01)Pair.cpp

---
+++ 01)Pair.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int,string>p;
+    // p = make_pair(2,"abc");
+    p = {1,"xyz"};
+    
+    cout<<p.first<<" "<<p.second<<endl;
+    
+    return 0;
+}
