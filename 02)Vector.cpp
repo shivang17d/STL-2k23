0afc4a278d6a424d0234e581cfaa7b9cdab063cf
Shivang Gupta <shivangguptaxd@gmail.com>
2023-07-08 04:29:59 -0700

    Update 02)Vector.cpp

--- 02)Vector.cpp
+++ 02)Vector.cpp
@@ -126,6 +126,40 @@ int main(){
 
 
 
+#include<bits/stdc++.h>
+using namespace std;
+void  printv(vector<pair<int,int>>&v){
+    for(int i = 0 ; i < v.size() ; ++i){
+        cout<<v[i].first<<" "<<v[i].second<<endl;
+    }
+    cout<<endl;
+}
+int main(){
+    vector<pair<int,int>>v={{1,2},{2,3},{3,1}};
+    printv(v);
+    
+    cout<<endl;
+    
+    vector<pair<int,int>>v1;
+    int n;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++) {
+        int x,y;
+        cin>>x>>y;
+        v1.push_back({x,y});
+    }
+    printv(v1);
+  
+    
+    return 0;
+}
+
+
+
+
+
+
+
 
 
 
