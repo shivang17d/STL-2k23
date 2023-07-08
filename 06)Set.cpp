75a6846d526fbe44efe9a28bc05934de746b833d
Shivang Gupta <shivangguptaxd@gmail.com>
2023-07-08 06:48:59 -0700

    Update 06)Set.cpp

--- 06)Set.cpp
+++ 06)Set.cpp
@@ -1,1 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    set<string> s;
+    int n;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++) {
+        string str;
+        cin>> str;
+        s.insert(str);
+    }
+    cout<<"Output"<<endl;
+    for(auto &val : s) {
+        cout<<val<<endl;
+    }
+
+    return 0;
+}
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unordered_set<string> s;
+    int n;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++) {
+        string str;
+        cin>> str;
+        s.insert(str);
+    }
+    cout<<"Output"<<endl;
+    for(auto &val : s) {
+        cout<<val<<endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    multiset<string> s;
+    int n;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++) {
+        string str;
+        cin>> str;
+        s.insert(str);
+    }
+    cout<<"Output"<<endl;
+    for(auto &val : s) {
+        cout<<val<<endl;
+    }
+
+    return 0;
+}
